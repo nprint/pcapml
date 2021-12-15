50965ed5d21587094ef4c24d98a94395c84b9665
Paul Schmitt <paul.schmitt@gmail.com>
2021-12-14 17:53:02 -0800

    renamed module, fixed dependency names

--- pypcapml/setup.py
+++ pypcapml/setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 
-pypcapml = Extension(name='pypcapml',
+pcapml_FE = Extension(name='pcapml_FE',
                      define_macros = [('MAJOR_VERSION', '0'),
                                       ('MINOR_VERSION', '1')],
 
@@ -8,16 +8,16 @@ pypcapml = Extension(name='pypcapml',
                                    '../include/sample/', '../include/pcap/',
                                    '../include/pcapng/'],
                      libraries=['pcap', 'stdc++'],
-                     sources=['pcapml_pymod.cpp', '../src/pcapng/block.cpp',
-                              '../src/pcapng/pcapng_reader.cpp', '../src/pcapng/pcapng_sorter.cpp',
+                     sources=['pcapml_pymod.cpp', '../src/pcapng/block_pcapng.cpp',
+                              '../src/pcapng/reader_pcapng.cpp', '../src/pcapng/sorter_pcapng.cpp',
                               '../src/util.cpp', '../src/sample/sampler.cpp',
                               '../src/sample/sample.cpp']
                     )
 
-setup(name="pypcapML",
+setup(name="pcapml_FE",
       version="0.0.1",
       description="Python interface for pcapML",
       author="nPrint",
       author_email="nprint.pr@gmail.com",
-      ext_modules=[pypcapml]
+      ext_modules=[pcapml_FE]
      )
